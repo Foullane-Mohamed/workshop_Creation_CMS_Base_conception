42ef52342a4d496bfd0609d4d4d49583a21f3546
hamza <hamzaprogeame@gmail.com>
2024-12-23 09:47:02 +0100

    ajouter les tableau du tags et article_tags

--- db.sql
+++ db.sql
@@ -7,3 +7,18 @@ USE CMS ;
 CREATE TABLE categories (
     categories_id int();
 )
+
+CREATE TABLE `tags` (
+  `ID` INT(11) AUTO_INCREMENT PRIMARY KEY,
+  `name` VARCHAR(255) NOT NULL UNIQUE,
+  `created_at` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+);
+
+CREATE TABLE `article_tags` (
+  `article_id` INT(11) NOT NULL,
+  `tag_id` INT(11) NOT NULL,
+  PRIMARY KEY (`article_id`, `tag_id`),
+  FOREIGN KEY (`article_id`) REFERENCES `articles`(`ID`) ON DELETE CASCADE,
+  FOREIGN KEY (`tag_id`) REFERENCES `tags`(`ID`) ON DELETE CASCADE
+);
+
