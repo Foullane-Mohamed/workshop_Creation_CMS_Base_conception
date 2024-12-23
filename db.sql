ed23eafad5a3ea22bfe0cca927d2c585c379adfd
‪Mohamed Foullane‬‏ <142094528+Foullane-Mohamed@users.noreply.github.com>
2024-12-23 09:59:37 +0100

    ajouter time to table articles

--- db.sql
+++ db.sql
@@ -6,6 +6,14 @@ CREATE DATABASE CMS;
 
 USE CMS;
 
+CREATE Table users (
+    user_id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    user_name varchar(50),
+    profile varchar(50),
+    bio varchar(255),
+    password varchar(50),
+    email varchar(255)
+)
 
 CREATE TABLE categories (
     categories_id int(11) NOT NULL AUTO_INCREMENT,
@@ -16,29 +24,18 @@ CREATE TABLE categories (
 )
 
 CREATE TABLE `tags` (
-  `tag_id` INT(11) AUTO_INCREMENT PRIMARY KEY,
-  `name` VARCHAR(255) NOT NULL UNIQUE,
-  `created_at` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    `tag_id` INT(11) AUTO_INCREMENT PRIMARY KEY,
+    `name` VARCHAR(255) NOT NULL UNIQUE,
+    `created_at` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
 );
 
 CREATE TABLE `article_tags` (
-  `article_id` INT(11) NOT NULL,
-  `tag_id` INT(11) NOT NULL,
-  PRIMARY KEY (`article_id`, `tag_id`),
-  FOREIGN KEY (`article_id`) REFERENCES `articles`(`article_id `) ON DELETE CASCADE,
-  FOREIGN KEY (`tag_id`) REFERENCES `tags`(`tag_id`) ON DELETE CASCADE
+    `article_id` INT(11) NOT NULL,
+    `tag_id` INT(11) NOT NULL,
+    PRIMARY KEY (`article_id`, `tag_id`),
+    FOREIGN KEY (`article_id`) REFERENCES `articles` (`article_id `) ON DELETE CASCADE,
+    FOREIGN KEY (`tag_id`) REFERENCES `tags` (`tag_id`) ON DELETE CASCADE
 );
-
-
-
-CREATE Table users(
-    user_id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
-    user_name varchar(50),
-    profile varchar(50),
-    bio varchar(255),
-    password varchar (50),
-    email varchar(255)
-)
 
 CREATE TABLE articles (
     article_id INT AUTO_INCREMENT PRIMARY KEY,
@@ -46,8 +43,10 @@ CREATE TABLE articles (
     content TEXT NOT NULL,
     status ENUM('brouillon', 'publie') DEFAULT 'draft',
     views INT DEFAULT 0,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
     user_id INT NOT NULL,
     category_id INT NOT NULL,
-    FOREIGN KEY (user_id) REFERENCES users(user_id) ON DELETE CASCADE  ON UPDATE CASCADE,
-    FOREIGN KEY (category_id) REFERENCES categories(category_id) ON DELETE CASCADE  ON UPDATE CASCADE
+    FOREIGN KEY (user_id) REFERENCES users (user_id) ON DELETE CASCADE ON UPDATE CASCADE,
+    FOREIGN KEY (category_id) REFERENCES categories (category_id) ON DELETE CASCADE ON UPDATE CASCADE
 );
