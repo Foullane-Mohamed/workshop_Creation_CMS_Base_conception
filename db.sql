13dd267a2baae1be4c8b9c2070a3a194f07fc232
‪Mohamed Foullane‬‏ <142094528+Foullane-Mohamed@users.noreply.github.com>
2024-12-23 09:24:52 +0100

    ajouter table articles

--- db.sql
+++ db.sql
@@ -1,9 +1,24 @@
 -- db
 
 DROP DATABASE IF EXISTS CMS;
-CREATE DATABASE CMS ;
-USE CMS ;
 
-CREATE TABLE categories (
-    categories_id int();
+CREATE DATABASE CMS;
+
+USE CMS;
+
+CREATE TABLE categories ( categories_id int();
+
 )
+
+CREATE TABLE articles (
+    article_id INT AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(255) NOT NULL,
+    content TEXT NOT NULL,
+    status ENUM('brouillon', 'publie') DEFAULT 'draft',
+    
+    user_id INT NOT NULL,
+    category_id INT NOT NULL,
+
+    FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE,
+    FOREIGN KEY (category_id) REFERENCES categories (id) ON DELETE CASCADE
+);
