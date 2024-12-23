34e2ffdd80d085d2ba1e5c29349c04c4a21d8030
Mhamedaithssaine <mhamedaithssaine1@gmail.com>
2024-12-23 10:06:42 +0100

    supprimer article_id

--- db.sql
+++ db.sql
@@ -19,8 +19,6 @@ CREATE TABLE categories (
     categories_id int(11) NOT NULL AUTO_INCREMENT,
     categories VARCHAR(20),
     PRIMARY KEY (categories_id),
-    article_id int(11),
-    FOREIGN KEY (article_id) REFERENCES articles (article_id) ON DELETE CASCADE ON UPDATE CASCADE
 )
 
 CREATE TABLE `tags` (
